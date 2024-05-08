1895e7dc6d6bcf03aaf223c3af4e2ffaf21c4170
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-05-08 17:08:47 +0100

    Complete 1.3: Course Information step 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,21 +4,28 @@ import Total from '/src/components/Total';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 
   return (
     <div>
       <Header course={course}/>
-      <Part part={part1} number={exercises1}/>
-      <Part part={part2} number={exercises2}/>
-      <Part part={part3} number={exercises3}/>
-      <Total numbers={[exercises1, exercises2, exercises3]}/>
+      <Part part={part1.name} number={part1.exercises}/>
+      <Part part={part2.name} number={part2.exercises}/>
+      <Part part={part3.name} number={part3.exercises}/>
+
+      <Total numbers={[part1.exercises, part2.exercises, part3.exercises]}/>
     </div>
   )
 }
