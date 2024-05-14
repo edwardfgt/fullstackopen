999272c4faaf5c4b5391d666135d68f93597ff91
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-05-14 21:58:28 +0100

    Complete 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+    {parts.map((part) => {
+      return (
+        <Part part={part.name} exercises={part.exercises} key={part.id} />
+      )
+    })}
+    </div>
+
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App;
