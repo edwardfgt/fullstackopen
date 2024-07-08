7f7f53a00d389846c623a12978817c1852a2358d
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-07-08 22:39:10 +0100

    Complete 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const max = Math.max(...blogs.map((blog) => blog.likes));
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
